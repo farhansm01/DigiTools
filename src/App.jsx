b6219da5c165b41b0960877e1b7cf3d1409e468a
farhansm01 <farhansadiq2021@gmail.com>
2026-04-01 18:31:31 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { FaBeer } from "react-icons/fa";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-black text-white text-4xl text-center py-6">
+        Hello World!!
+      </h1>
+      <FaBeer />
+    </>
+  );
+}
+
+export default App;
